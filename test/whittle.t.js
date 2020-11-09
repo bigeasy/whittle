ffb003566fb60385007c71629f727da8d5e7a963
Alan Gutierrez <alan@prettyrobots.com>
2020-11-09 04:59:45 -0600

    Implement compared value extraction.
    Extracting the values to compare is a separate concern from comparing
    them. I'm constantly fussing with the shape of comparator arguments in
    the b-tree and MVCC libraries. Ascension binds a single extraction
    strategy but I'm often overriding that strategy, so now Ascension will
    bind no extraction strategy and we'll whittle arguments as necessary.
    
    Closes #6.

--- test/whittle.t.js
+++ test/whittle.t.js
@@ -1,4 +1,10 @@
 require('proof')(1, okay => {
     const whittle = require('..')
-    okay(whittle, 'require')
+    function comparator (left, right) {
+        return left - right
+    }
+
+    const whittled = whittle(comparator, object => object[0])
+
+    okay(whittled([ 1 ], [ 1 ]), 0, 'whittled')
 })
--- whittle.js
+++ whittle.js
@@ -1,1 +1,5 @@
-module.exports = 1
+module.exports = function (comparator, whittle) {
+    return function (left, right) {
+        return comparator(whittle(left), whittle(right))
+    }
+}
