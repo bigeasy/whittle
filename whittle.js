e62d28609c53c914ef5d24726104975e439403ca
Alan Gutierrez <alan@prettyrobots.com>
2021-10-18 02:14:10 -0500

    Accommodate binary search comparator reversal.

--- whittle.js
+++ whittle.js
@@ -1,7 +1,7 @@
 module.exports = function (comparator, ...vargs) {
     const whittleLeft = vargs.shift()
     const whittleRight = vargs.shift() || whittleLeft
-    return function (left, right) {
-        return comparator(whittleLeft(left), whittleRight(right))
+    return function (left, right, reversal) {
+        return comparator(whittleLeft(left), whittleRight(right), reversal)
     }
 }
