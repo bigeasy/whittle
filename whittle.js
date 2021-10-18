86a7baa145059757f7cc7144da13beff08ca7a61
Alan Gutierrez <alan@prettyrobots.com>
2021-10-18 02:18:42 -0500

    Binary search comparator optional.

--- whittle.js
+++ whittle.js
@@ -1,7 +1,13 @@
 module.exports = function (comparator, ...vargs) {
+    const reversal = typeof vargs[vargs.length - 1] == 'boolean' ? vargs.pop() : false
     const whittleLeft = vargs.shift()
     const whittleRight = vargs.shift() || whittleLeft
-    return function (left, right, reversal) {
+    if (reversal) {
+        return function (left, right, reversal) {
+            return comparator(whittleLeft(left), whittleRight(right), reversal)
+        }
+    }
+    return function (left, right) {
         return comparator(whittleLeft(left), whittleRight(right), reversal)
     }
 }
